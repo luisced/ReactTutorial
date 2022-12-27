47f0f67fde8f8179cff1098be991b129cb8af9d1
luisced <luis.cedillo.maldonado@gmail.com>
2022-12-27 14:29:14 -0600

    addded testimonios-free-code-camp

---
+++ testimonios-free-code-camp/src/components/Testimony.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Testimony = () => {
+	return (
+		<div className="testimony_container">
+			<img
+				src={require("../images/testimony1.jpeg")}
+				alt="Foto de Actor"
+				className="testimony_image"
+			/>
+			<div className="testimony_text_container">
+				<p className="testimony_name">The Rock</p>
+				<p className="testimony_role">Chief</p>
+				<p className="testimony_text">
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque,
+					aut similique? Dolorem delectus ipsam maiores exercitationem
+					dignissimos laudantium nam excepturi? Labore, unde? Odit fuga iste
+					maxime ratione corrupti ipsum magnam.
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default Testimony;
