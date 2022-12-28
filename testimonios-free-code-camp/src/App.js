ad888309596f579c96dca1f0b5fc388b23bb63b6
luisced <luis.cedillo.maldonado@gmail.com>
2022-12-27 22:39:31 -0600

    addded testimonios-free-code-camp

--- testimonios-free-code-camp/src/App.js
+++ testimonios-free-code-camp/src/App.js
@@ -10,7 +10,30 @@ function App() {
 					working full time
 				</h1>
 			</div>
-			<Testimony />
+			<Testimony
+				name="The Rock"
+				country="USA"
+				image="1"
+				role="Software Engineer"
+				enterprise="Spotify"
+				testimony="I was able to finish the entire freeCodeCamp curriculum in 9 months while working full time. I was able to get a job as a software engineer at Spotify."
+			/>
+			<Testimony
+				name="Chris"
+				country="USA"
+				image="2"
+				role="Software Engineer"
+				enterprise="Google"
+				testimony="I was able to finish the entire freeCodeCamp curriculum in 9 months while working full time. I was able to get a job as a software engineer at Google."
+			/>
+			<Testimony
+				name="Will"
+				country="Mexico"
+				image="3"
+				role="Software Engineer"
+				enterprise="Facebook"
+				testimony="I was able to finish the entire freeCodeCamp curriculum in 9 months while working full time. I was able to get a job as a software engineer at Facebook."
+			/>
 		</div>
 	);
 }
--- testimonios-free-code-camp/src/components/Testimony.jsx
+++ testimonios-free-code-camp/src/components/Testimony.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 import "../styles/Testimony.css";
 
-export const Testimony = () => {
+export const Testimony = (props) => {
 	return (
 		<div className="testimony_container">
 			<img
-				src={require("../images/testimony1.jpeg")}
+				src={require(`../images/actor-${props.image}.jpg`)}
 				alt="Foto de Actor"
 				className="testimony_image"
 			/>
 			<div className="testimony_text_container">
-				<p className="testimony_name">The Rock</p>
-				<p className="testimony_role">Chief</p>
-				<p className="testimony_text">
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque,
-					aut similique? Dolorem delectus ipsam maiores exercitationem
-					dignissimos laudantium nam excepturi? Labore, unde? Odit fuga iste
-					maxime ratione corrupti ipsum magnam.
+				<p className="testimony_name">
+					{props.name} in {props.country}
 				</p>
+				<p className="testimony_role">
+					{props.role} in {props.enterprise}
+				</p>
+				<p className="testimony_text">"{props.testimony}"</p>
 			</div>
 		</div>
 	);
